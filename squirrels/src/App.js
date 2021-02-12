bc36ed1de96e2759f20a871c118d04bbc3b014a8
SantiagoVira <8bitharrypotter@gmail.com>
2021-02-12 13:37:32 -0500

    Fixed errors

--- squirrels/src/App.js
+++ squirrels/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <h1>Hello World!</h1>
+            <div className="bottom"></div>
+        </div>
+    );
 }
 
 export default App;
