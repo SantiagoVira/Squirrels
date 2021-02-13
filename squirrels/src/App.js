acd6540eb296f9e6487194c52d05ba71eed785f2
SantiagoVira <8bitharrypotter@gmail.com>
2021-02-13 16:35:01 -0500

    Sciurinae (Make the title bar)

--- squirrels/src/App.js
+++ squirrels/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Bottom from "./Bottom";
+import Top from "./Top";
 
 function App() {
     return (
         <div className="App">
+            <Top />
             <h1>Hello World!</h1>
             <h1>Hello World!</h1>
             <h1>Hello World!</h1>
---
+++ squirrels/src/Top.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+
+function Top() {
+    return (
+        <div className="topContainer">
+            <svg className="top" viewBox="0 0 500 100">
+                <path d="M0, 50 C150, 150 350, 30 550, 85 L500, 0 L0, 0 Z" />
+            </svg>
+            <h1 className="title">SQRRLZ</h1>
+        </div>
+    );
+}
+
+export default Top;
