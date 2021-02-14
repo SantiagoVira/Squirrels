6ed492f402fe438923a87603190b7472c8584ac0
Zetta56 <67248032+Zetta56@users.noreply.github.com>
2021-02-14 17:17:42 -0500

    fix story fetching

--- squirrels/src/Gallery/Gallery.js
+++ squirrels/src/Gallery/Gallery.js
@@ -1,47 +1,40 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 
 import Card from "./Card/Card.js";
 import "./Gallery.css";
 
-function getSquirrelStories() {
-  return axios({
-    method: 'get',
-    url: 'https://data.cityofnewyork.us/resource/gfqj-f768.json',
-  })
-  .then(function(response) {
-    console.log(response.data[
-      Math.floor(Math.random() * Math.floor(response.data.length-1))
-    ].note_squirrel_park_stories) // I like seeing random squirrel stories :)
-
-    return response;
-  });
-}
-
 function RenderSquirrels(props) {
-  return <Card
-    title="This would be the story category"
-    content="I'm too tired to fix the import rn but here's a sample simple card"
-  />
-  // Render the stories, raises: can't render an object
-  // return props.stories.then(function(response) {
-  //   return (
-  //     response.data.map((log) => {
-  //       return (
-  //         <Card
-  //           // title={whatever part of the log starts with "story_topic"}
-  //           content={log.note_squirrel_park_stories}
-  //         />
-  //       );
-  //     })
-  //   );
-  // })
+  //Render the stories, raises: can't render an object
+  return props.stories.map((log) => {
+    return (
+      <Card
+        //key={some kind of unique identifier}
+        //title={whatever part of the log starts with "story_topic"}
+        content={log.note_squirrel_park_stories}
+      />
+    );
+  })
 }
 
 function Gallery() {
+  const [stories, setStories] = useState(null);
+
+  useEffect(async () => {
+    const response = await axios({
+      method: 'get',
+      url: 'https://data.cityofnewyork.us/resource/gfqj-f768.json',
+    });
+    setStories(response.data);
+  });
+
+  if(!stories) {
+    return null;
+  }
+
   return (
     <div>
-      <RenderSquirrels stories={getSquirrelStories()} />
+      <RenderSquirrels stories={stories} />
     </div>
   );
 }
