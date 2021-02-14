f4c27d62b50c728e164caba53cae4bf6aa8af922
SantiagoVira <8bitharrypotter@gmail.com>
2021-02-14 12:52:02 -0500

    the menu almost updates based on the link chosen

--- squirrels/src/Menu/Menu.js
+++ squirrels/src/Menu/Menu.js
@@ -1,25 +1,53 @@
-import React, {useState, useEffect} from "react";
-import {Link} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./Menu.css";
 
 function Menu() {
     const [scrolled, setScrolled] = useState("");
+    const [page, setPage] = useState(window.location.href);
 
     useEffect(() => {
         window.addEventListener("scroll", onScroll);
-    })
+    });
 
     const onScroll = () => {
-        window.pageYOffset > 0 ? setScrolled("scrolled") : setScrolled("")
+        window.pageYOffset > 0 ? setScrolled("scrolled") : setScrolled("");
+    };
+
+    function unique() {
+        return Math.floor(
+            Math.random() * Math.floor(Math.random() * Date.now())
+        ).toString();
     }
+
+    const links = [
+        { to: "/", name: "Squirrels" },
+        { to: "/gallery", name: "Gallery" },
+        { to: "/uploads", name: "Uploads" },
+    ];
 
     return (
         <div className={`menu ${scrolled}`}>
-            <strong><Link to="/">Squirrels</Link></strong>
-            <Link to="/gallery">Gallery</Link>
-            <Link to="/uploads">Uploads</Link>
+            {links.map((link) =>
+                !page.endsWith(link.to) ? (
+                    <strong key={unique()}>
+                        <Link to={link.to}>{link.name}</Link>
+                    </strong>
+                ) : (
+                    <Link
+                        key={unique()}
+                        to={link.to}
+                        onClick={() => {
+                            setPage(window.location.href);
+                        }}
+                    >
+                        {link.name}
+                    </Link>
+                )
+            )}
         </div>
     );
 }
 
 export default Menu;
+//change which is stong based on seleccted
