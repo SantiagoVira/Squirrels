0bd75c79e2d74d13af57b003a00271764485feee
SantiagoVira <8bitharrypotter@gmail.com>
2021-02-16 12:04:22 -0500

    created banner logo and other little things

--- client/src/About/About.js
+++ client/src/About/About.js
@@ -20,20 +20,20 @@ function About() {
                     <br />
                     <br />
                     In early February 2021, 4 young programmers were tasked with
-                    creating a project to [How would yall phrase it]. As they
-                    were looking through the provided datasets, the stumbled
-                    across the perfect idea. In 2018, the New York City
-                    Government conducted a squirrel census in Central Park,
-                    recording sightings of squirrels as well as their colors and
-                    attitudes towards the census takers. As the group was
-                    laughing at how absurd this was, the idea was formed. They
-                    realized they could share the mental health boost provided
-                    by the absurdity of a squirrel census with the rest of the
-                    world. Just as they thought it couldn't get any better, well
-                    you can probably guess. It got better. There was another
-                    dataset about squirrels. But this one, this one was about
-                    the stories. The events that took place during a sighting of
-                    a squirrel. Neatly tagged and packed into categories, these
+                    creating a project to solve a health problem. As they were
+                    looking through the provided datasets, the stumbled across
+                    the perfect idea. In 2018, the New York City Government
+                    conducted a squirrel census in Central Park, recording
+                    sightings of squirrels as well as their colors and attitudes
+                    towards the census takers. As the group was laughing at how
+                    absurd this was, the idea was formed. They realized they
+                    could share the mental health boost provided by the
+                    absurdity of a squirrel census with the rest of the world.
+                    Just as they thought it couldn't get any better, well you
+                    can probably guess. It got better. There was another dataset
+                    about squirrels. But this one, this one was about the
+                    stories. The events that took place during a sighting of a
+                    squirrel. Neatly tagged and packed into categories, these
                     stories were often ridiculous, and some didn't even relate
                     to squirrels at all.
                     <br />
--- client/src/Footer/Footer.js
+++ client/src/Footer/Footer.js
@@ -6,7 +6,13 @@ function Footer() {
         <div>
             <div style={{ height: "20vh" }}></div>
             <div className="footer">
-                <p>Test text</p>
+                <p>
+                    Sqrrlz was made by 4 talented idiots who chose to make an
+                    <br />
+                    app to share pictures and stories of tree rodents rather
+                    <br />
+                    than... literaly anything else
+                </p>
             </div>
         </div>
     );
