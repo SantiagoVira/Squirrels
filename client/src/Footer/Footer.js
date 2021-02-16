7248e9796a39ad2ded9951fa56603690aa5a2c8f
SantiagoVira <8bitharrypotter@gmail.com>
2021-02-16 12:52:36 -0500

    footer

--- client/src/Footer/Footer.js
+++ client/src/Footer/Footer.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Footer.css";
+import Banner from "./banner logo 2.png";
 
 function Footer() {
     return (
         <div>
             <div style={{ height: "20vh" }}></div>
             <div className="footer">
+                <img src={Banner} alt="Sqrrlz" />
                 <p>
                     Sqrrlz was made by 4 talented idiots who chose to make an
                     <br />
