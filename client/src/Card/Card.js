189e3005762cc5b18143835388b4cad953bf1a94
SantiagoVira <8bitharrypotter@gmail.com>
2021-02-17 14:02:15 -0500

    arrows

--- client/src/Card/Card.js
+++ client/src/Card/Card.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Card.css";
 
 function Card(props) {
+    //When we call the card component, pass the id to access it on the server
+    const [votes, setVotes] = useState(1);
+
+    function vote(id, op) {
+        const changer = op === "up" ? 1 : -1;
+        const newTotal = votes + changer;
+        setVotes(newTotal);
+        //access card at [id]
+        //Set card's votes in the database to votes variable
+    }
     return (
         <div className="squirrelCard">
             <h1>{props.title}</h1>
             <p>{props.content}</p>
+            <div className="buttons">
+                <div
+                    onClick={() => {
+                        vote(props.id, "up");
+                    }}
+                    className="up"
+                ></div>
+                <p className="votes">{votes}</p>
+                <div
+                    onClick={() => {
+                        vote(props.id, "down");
+                    }}
+                    className="down"
+                ></div>
+            </div>
         </div>
     );
 }
