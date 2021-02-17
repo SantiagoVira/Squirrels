9b1a0385a388e8246c51c9b3f9f8d520581ef87a
Dashiell Elliott <dashiell.elliott@yahoo.com>
2021-02-17 13:17:49 -0500

    update squirrel log model

--- server/api/models.py
+++ server/api/models.py
@@ -5,6 +5,8 @@ class SquirreLog(models.Model):
     topic = models.CharField(max_length=100)
     note = models.TextField(max_length=400) # Arbitrary length of a note
     pub_date = models.DateTimeField('date published')
+    user_story = models.BooleanField(default=False)
+    votes = models.IntegerField(default=0)
     # Maybe a field for squirrel upvotes from users?
     # Reporting system  
 
