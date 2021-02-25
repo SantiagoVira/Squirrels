321f3705aaa7aa696e2759cc9533f5c22052654f
Aramie <aramie.ewen@gmail.com>
2021-02-25 00:02:52 -0500

    Center footer content

--- client/src/Footer/Footer.js
+++ client/src/Footer/Footer.js
@@ -2,24 +2,24 @@ import React from "react";
 import "./Footer.css";
 import Banner from "./banner logo 2.png";
 import { Link } from "react-router-dom";
-import Row from "../Row";
+// import Row from "../Row";
 
 function Footer() {
     return (
-        <div>
-            <div style={{ height: "20vh" }}></div>
-            <Row className="footer">
-                <img className="bannerImg" src={Banner} alt="Sqrrlz" />
-                <p className="footerBlurb">
-                    Sqrrlz was made by{" "}
-                    <Link to="/about">4 talented idiots</Link> who chose to make
-                    an app to share pictures and stories of tree rodents rather
-                    than... literally anything else. <br />
-                    But, let's be honest, nothing is better than "squirrels"
-                    spelled in a hip way.
-                </p>
-            </Row>
+      <>
+        {/* There has to be a cleaner way to style that... right?? */}
+        <div style={{ height: "20vh" }}></div>
+        <div className="footer">
+            <p className="footerBlurb">
+                Sqrrlz was made by <Link to="/about">4 talented idiots</Link> who chose to make
+                an app to share tree rodents rather
+                than... literally anything else. <br />
+                But, let's be honest, nothing is better than "squirrels"
+                spelled in a hip way.
+            </p>
+            <img className="bannerImg" src={Banner} alt="Sqrrlz" />
         </div>
+      </>
     );
 }
 
