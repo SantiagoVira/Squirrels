19c6d384a794a84dbd97daaae8117011a6309262
Dashiell Elliott <dashiell.elliott@yahoo.com>
2021-04-26 16:51:10 -0400

    update linux run script

--- run.sh
+++ run.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-cd client && npm run build && cd ../server && python manage.py runserver && cd ..
+function cleanup ()
+{
+    cd ..
+    docker stop "$c"
+    exit
+}
+
+trap "cleanup" 2
+
+c="$1"
+docker start "$c"
+
+if [ "$2" = "rebuild" ]; then
+    cd client
+    npm run build
+    cd ..
+fi
+
+cd server
+python manage.py runserver
